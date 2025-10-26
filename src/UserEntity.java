b91febea08e257198dd0efa8ed04e1288e3b3ea8
Filip Stevanovic <filip.stevanovic2016@gmail.com>
2025-10-26 02:02:34 +0200

    added sout

---
+++ src/UserEntity.java
@@ -0,0 +1,33 @@
+public class UserEntity {
+    int age;
+    String name;
+
+    public UserEntity(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
